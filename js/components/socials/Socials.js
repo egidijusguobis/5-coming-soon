ab7f6ad0a9040651cc1521ce3999c035935f6b66
Egidijus <egidijusx@gmail.com>
2021-08-17 15:27:54 +0300

    Social icons with validation

--- js/components/socials/Socials.js
+++ js/components/socials/Socials.js
@@ -12,14 +12,29 @@ class Socials {
             return false;
         }
 
+        if (!this.findTargetElement()) {
+            console.error('ERROR: pagal pateikta this.selector nepavyko rasti norimo elemento');
+            return false;
+        }
+
         this.render();
     }
 
     isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+        this.selector === '') {
+            console.error('ERROR: this.selector turi buti ne tuscias tekstas');
+            return false;
+        }
         return true;
     }
 
     isValidData() {
+        if (typeof this.selector !== 'string' ||
+        this.selector === '') {
+            console.error('ERROR: this.selector turi buti ne tuscias tekstas');
+            return false;
+        }
         return true;
     }
 
@@ -29,6 +44,37 @@ class Socials {
     }
 
     isValidDataItem(item) {
+        const allowedKeys = ['href', 'icon'];
+
+        // tikriname, jog item yra objektas
+        if (typeof item !== 'object' ||
+            Array.isArray(item) ||
+            item === null) {
+            console.warn('WARN: netinkamas tipas');
+            return false;
+        }
+
+        // tikriname, ar item.href yra tinkamas
+        if (typeof item.href !== 'string' ||
+            item.href === '') {
+            console.warn('WARN: netinkamas item.label');
+            return false;
+        }
+
+        // tikriname, ar item.icon yra tinkamas
+        if (typeof item.icon !== 'string' ||
+            item.icon === '') {
+            console.warn('WARN: netinkamas item.label');
+            return false;
+        }
+
+        // tikriname, ar item objektas neturi per daug key's (raktazodziu)
+        for (const key in item) {
+            if (!allowedKeys.includes(key)) {
+                return false;
+            }
+        } 
+
         return true;
     }
 
