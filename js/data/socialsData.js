43d2b794f0168a78631f512a165be04b8067c660
Egidijus <egidijusx@gmail.com>
2021-08-17 15:10:19 +0300

    social icons w/o validation

--- js/data/socialsData.js
+++ js/data/socialsData.js
@@ -1,3 +1,32 @@
-const socialsData = [];
+const socialsData = [
+    {
+        href: '#',
+        icon: 'facebook',
+    },
+    {
+        href: '#',
+        icon: 'twitter',
+    },
+    {
+        href: '#',
+        icon: 'dribbble',
+    },
+    {
+        href: '#',
+        icon: 'tumblr',
+    },
+    {
+        href: '#',
+        icon: 'linkedin',
+    },
+    {
+        href: '#',
+        icon: 'instagram',
+    },
+    {
+        href: '#',
+        icon: 'rss',
+    },
+];
 
 export { socialsData }
